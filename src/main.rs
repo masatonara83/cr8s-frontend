66bbb45c6cb74faca3dba91684e7f7d93ef2f805
masatonara83 <masato.nara@rakus-partners.co.jp>
2023-06-22 17:39:30 +0900

    first comit

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <p>{"Hello World"}</p>
+    }
+}
+
+fn main() {
+    yew::Renderer::<App>::new().render();
+}
