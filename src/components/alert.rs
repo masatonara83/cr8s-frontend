315488c5e4fd22df58b32d6ac25a22cf5a3f3643
masatonara83 <masato.nara@rakus-partners.co.jp>
2023-06-23 16:38:18 +0900

    ログインエンドポイントとの通信完了

---
+++ src/components/alert.rs
@@ -0,0 +1,28 @@
+use yew::prelude::*;
+
+#[derive(Properties, PartialEq)]
+pub struct Props {
+    pub alert_type: AlertType,
+    pub message: AttrValue,
+}
+
+#[derive(PartialEq)]
+pub enum AlertType {
+    _Info,
+    _Success,
+    Danger,
+}
+
+#[function_component(Alert)]
+pub fn alert(props: &Props) -> Html {
+    let alert_type = match props.alert_type {
+        AlertType::_Info => "info".to_string(),
+        AlertType::_Success => "success".to_string(),
+        AlertType::Danger => "danger".to_string(),
+    };
+    html! {
+      <div class={format!("alert alert-{}", alert_type)} role="alert">
+        {props.message.clone()}
+      </div>
+    }
+}
--- src/components/login_form.rs
+++ src/components/login_form.rs
@@ -1,8 +1,20 @@
 use gloo_console::log;
 use web_sys::HtmlInputElement;
-use yew::prelude::*;
+use yew::{platform::spawn_local, prelude::*};
 
-use crate::components::input::*;
+use crate::api::user::{api_login, api_me, LoginResponse, MeResponse};
+
+use crate::components::alert::Alert;
+use crate::components::alert::AlertType;
+use crate::components::input::Input;
+async fn login(
+    username: String,
+    password: String,
+) -> Result<(LoginResponse, MeResponse), gloo_net::Error> {
+    let login_response = api_login(username, password).await?;
+    let me_response = api_me(&login_response.token).await?;
+    Ok((login_response, me_response))
+}
 
 #[function_component(LoginForm)]
 pub fn login_form() -> Html {
@@ -11,6 +23,9 @@ pub fn login_form() -> Html {
 
     let password_handle = use_state(String::default);
     let password = (*password_handle).clone();
+
+    let error_message_handle = use_state(String::default);
+    let error_message = (*error_message_handle).clone();
 
     let username_changed = Callback::from(move |e: Event| {
         let target = e.target_dyn_into::<HtmlInputElement>();
@@ -31,15 +46,26 @@ pub fn login_form() -> Html {
 
     let onsubmit = Callback::from(move |e: SubmitEvent| {
         e.prevent_default();
-        log!(
-            "Subminting form",
-            cloned_username.clone(),
-            cloned_password.clone()
-        );
+
+        let cloned_username = cloned_username.clone();
+        let cloned_password = cloned_password.clone();
+        let cloned_error_handle = error_message_handle.clone();
+        spawn_local(async move {
+            match login(cloned_username.clone(), cloned_password.clone()).await {
+                Ok(responses) => log!(responses.1.username),
+                Err(e) => cloned_error_handle.set(e.to_string()),
+            }
+        });
     });
 
     html! {
       <form onsubmit={onsubmit}>
+        if error_message.len() > 0 {
+          < Alert
+            alert_type={AlertType::Danger}
+            message={error_message}
+          />
+        }
         <div class="mb-3">
           <Input
             input_type="text"
