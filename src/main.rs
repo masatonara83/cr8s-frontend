de8959a2ce0386d7a52df1afa0e4ecb481695bb8
masatonara83 <masato.nara@rakus-partners.co.jp>
2023-06-26 10:26:09 +0900

    Routerによるページ分岐を追加

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,48 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
 
 mod api;
 mod components;
 mod pages;
 
+#[derive(Routable, PartialEq, Clone)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[at("/rustacreans")]
+    Rustaceans,
+    #[at("/crates")]
+    Crates,
+    #[at("/login")]
+    Login,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(route: Route) -> Html {
+    match route {
+        Route::Home => html! {
+            <pages::home::Home />
+        },
+        Route::Login => html! {
+            <pages::login::Login />
+        },
+        Route::NotFound => html! {
+            <pages::not_found::NotFound />
+        },
+        _ => html! {
+            <pages::login::Login />
+        },
+    }
+}
+
 #[function_component(App)]
 fn app() -> Html {
     html! {
-        <pages::login::Login />
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
     }
 }
 
